c18c49195ab549789c31d518e28e359258422a4b
Harry <hjwp2@cantab.net>
2013-07-23 11:26:21 +0000

    adds a shell script to use bonnie++ instead

---
+++ run-bonnie-tests.sh
@@ -0,0 +1,21 @@
+if [ -z "$1" ]
+    then
+        TARG=/mnt/user_storage/bonnie/from-`hostname`
+    else
+        TARG=$1/bonnie/from-`hostname`
+fi
+
+for subdir in d1 d2 d3
+do
+    echo "creating dir for test at " $TARG/$subdir
+    mkdir -p $TARG/$subdir
+done
+
+chmod -R 777 $TARG
+
+bonnie++ -u nobody -p -1
+bonnie++ -u nobody -p 3
+for subdir in d1 d2 d3
+do
+    bonnie++ -u nobody -ys -d $TARG/$subdir -n 4 -s 4096 > out$subdir.csv &
+done
